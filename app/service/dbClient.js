27a8460c4f13166289d4dd2c1c535f6248389eee
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-05-04 16:35:30 +0200

    add client connection

--- app/service/dbClient.js
+++ app/service/dbClient.js
@@ -1,6 +1,11 @@
 import pkg from 'pg'; // equivalent strict à const Client=require('pg') 
 const { Client} = pkg;
-const client = new Client();
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });;
 client.connect();
 
 export default client;
